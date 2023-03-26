8a6923cf7cc1e68073fdaa6785cfea46dc4b534e
Wisnu Hidayat <wisnubaldas@gmail.com>
2023-03-27 04:17:54 +0700

    start

---
+++ application/middlewares/ApiMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Author: https://github.com/davinder17s
+ * Email: davinder17s@gmail.com
+ * Repository: https://github.com/davinder17s/codeigniter-middleware
+ */
+
+class ApiMiddleware {
+    protected $controller;
+    protected $ci;
+    public $roles = array();
+    public function __construct($controller, $ci)
+    {
+        $this->controller = $controller;
+        $this->ci = $ci;
+    }
+
+    public function run(){
+        $header = $this->ci->input->request_headers();
+        // $this->roles = array('somehting', 'view', 'edit');
+        header('Content-Type: application/json');
+        header('Status: 200');
+        header('HTTP/1.1: 200');
+        // echo json_encode($header);
+    }
+}
